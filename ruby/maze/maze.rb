06e3712cbdca1b6f9aaab8b19017ce91c14d88da
bsantanad <benjamin.santana@proton.me>
2024-11-03 14:05:59 -0600

    ruby/maze/.rb adding bfs and dfs algorithms
    super similar, just one line change, from stack to queue
    
    also added a helper function to print the path into the maze, but it does not
    work fully
    
    Signed-off-by: bsantanad <benjamin.santana@proton.me>

--- ruby/maze/maze.rb
+++ ruby/maze/maze.rb
@@ -81,6 +81,9 @@ class Maze
     ##
     # Given a list of squares, paint it in the matrix with X
     def paint_path(path)
+        # FIXME, you cannot call this function two times due to it overwriting
+        # the maze? or m? not entirely sure, need to look more into it.
+
         # TODO make this output html
         m = matrix.clone
         puts '----------'
@@ -154,4 +157,124 @@ def load_maze(filename)
     maze
 end
 
+##
+# Given a square, track all its previous squares.
+# Return a list with all the previous squares
+def get_path(square)
+    path = []
+    prev = square.previous
+    while prev != nil
+        path.push(prev)
+        prev = prev.previous
+    end
+    path
+end
+
+##
+# depth-first search
+#
+# over optimistic search algorithm, that hopes the next value it will look at
+# is the solution.
+#
+# it will store start by checking a square, if its the end of the maze, it will
+# return. If not it will get its neighbours and add them to a stack.
+#
+# for the next iteration it will pop one value from the stack and repeat the
+# process until it finds the solution.
+#
+# It will be biased for going to the right, since the order in which we add the
+# neighbours ends with the right, and since it is a stack, (first in, last out)
+# it will pop that value fist.
+#
+# If the maze has a solution, it is guaranteed to be found by this. But it does
+# not ensure the quality of the solution.
+def dfs(maze)
+    stack = []
+
+    start_square = maze.start
+    start_square.previous = nil
+    stack.push(start_square)
+
+    end_square = maze.end
+
+    visited_squares = [].to_set
+
+    while stack.length.positive?
+        square = stack.pop
+        if square == end_square
+            return square
+        end
+        visited_squares.add(square)
+
+        maze.neighbours(square).each do |neighbour|
+            # check if the neighbour is already in the set, if it is, we do not
+            # need to visit it again.
+            if visited_squares.include? neighbour or maze.is_wall(square)
+                next
+            end
+            neighbour.previous = square
+            stack.push(neighbour)
+        end
+        # binding.break
+    end
+
+    # if we ever get here, it means that we went thru the whole stack and did
+    # not find a solution.
+    return nil
+end
+
+
+##
+# breadth-first search
+#
+# pessimistic search algorithm, works exactly the same as dfs, but instead of a
+# stack uses a queue, this small change makes the algorithm methodical. Opposed
+# to dfs, which thinks is always one step away from the end of the maze. This
+# will first check all the neighbours, and until is finished with those, it
+# will go and check the neighbours of neighbours and so on.
+#
+# this one does guarantees that the path it finds is the shortest one.
+def bfs(maze)
+    queue = []
+
+    start_square = maze.start
+    start_square.previous = nil
+    queue.push(start_square)
+
+    end_square = maze.end
+
+    visited_squares = [].to_set
+
+    while queue.length.positive?
+        square = queue.first
+        queue.shift
+        if square == end_square
+            return square
+        end
+        visited_squares.add(square)
+
+        maze.neighbours(square).each do |neighbour|
+            # check if the neighbour is already in the set, if it is, we do not
+            # need to visit it again.
+            if visited_squares.include? neighbour or maze.is_wall(square)
+                next
+            end
+            neighbour.previous = square
+            queue.push(neighbour)
+        end
+        # binding.break
+    end
+
+    # if we ever get here, it means that we went thru the whole queue and did
+    # not find a solution.
+    return nil
+end
+
 maze = load_maze('sample.maze')
+dfs_path = get_path(dfs(maze))
+puts dfs_path.length
+
+bfs_path = get_path(bfs(maze))
+puts bfs_path.length
+
+maze.paint_path(bfs_path)
